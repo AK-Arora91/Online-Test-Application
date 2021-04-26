dc098e77f413e4e3080ae44f6778bc02fe35255c
ankarora <ankarora@teksystems.com>
2021-04-26 22:04:28 +0530

    sprint complete

---
+++ src/app/models/index.ts
@@ -0,0 +1,5 @@
+  
+export * from './option';
+export * from './question';
+export * from './quiz';
+export * from './quiz-config';
---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class QuizService {
+
+  constructor(private http: HttpClient) { }
+
+  get(url: string) {
+    return this.http.get(url);
+  }
+
+}
